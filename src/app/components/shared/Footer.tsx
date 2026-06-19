7c14425d299d3e516511e6d548fad2b80f3e3651
Md. Fayzur Rahman <mdfayzurrahman030@gmail.com>
2026-06-19 17:28:07 +0600

    peoblem fix

--- src/app/components/shared/Footer.tsx
+++ src/app/components/shared/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const socialLinks = [
     {
       name: 'Twitter',
-      href: 'https://twitter.com/OpenCompute',
+      href: 'https://x.com/',
       icon: (
         <svg className='w-3.5 h-3.5 fill-current' viewBox='0 0 24 24'>
           <path d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z' />
@@ -13,7 +13,7 @@ const Footer = () => {
     },
     {
       name: 'Facebook',
-      href: 'https://facebook.com/OpenCompute',
+      href: 'https://www.facebook.com/',
       icon: (
         <svg className='w-3.5 h-3.5 fill-current' viewBox='0 0 24 24'>
           <path d='M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z' />
@@ -22,7 +22,7 @@ const Footer = () => {
     },
     {
       name: 'Instagram',
-      href: 'https://instagram.com/OpenCompute',
+      href: 'https://www.instagram.com/',
       icon: (
         <svg
           className='w-3.5 h-3.5 stroke-current fill-none'
@@ -39,7 +39,7 @@ const Footer = () => {
     },
     {
       name: 'Linkedin',
-      href: 'https://linkedin.com/company/OpenCompute',
+      href: 'https://www.linkedin.com/',
       icon: (
         <svg className='w-3.5 h-3.5 fill-current' viewBox='0 0 24 24'>
           <path d='M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z' />
